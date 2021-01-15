410849babe39ef898ed3aae14c2288308774eada
Talha YÜKSEK <yuksek_talha@hotmail.com>
2021-01-15 13:46:18 +0300

    ..

--- src/app/Services/product.service.ts
+++ src/app/Services/product.service.ts
@@ -2,30 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { Product } from '../product/product';
 import { Observable, throwError } from 'rxjs';
-import {tap,catchError} from 'rxjs/operators'
+import { tap, catchError } from 'rxjs/operators'
 @Injectable()
 export class ProductService {
 
-  constructor( private http: HttpClient) { }
-  path="http://localhost:3000/products";
+  constructor(private http: HttpClient) { }
+  path = "http://localhost:3000/products";
 
-  getProducts():Observable<Product[]>{
-   return this.http.get<Product[]>(this.path).pipe(
-    tap(data=>console.log(JSON.stringify(data))),
-    catchError(this.handleError)
+  getProducts(categoryId): Observable<Product[]> {
 
-   );
+    var newPath = this.path;
+    if (categoryId) {
+      newPath += newPath + "?categoryId=" + categoryId;
+    }
+
+    return this.http.get<Product[]>(newPath).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+
+    );
 
   }
-  handleError(err: HttpErrorResponse){
-    let errorMessage=''
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = ''
 
-  if (err.error instanceof ErrorEvent) {
-    errorMessage='Bir Hata Oluştu'+err.error.message
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Bir Hata Oluştu' + err.error.message
 
-  }else{
-    errorMessage='Sistemsel bir Hata'
-  }
-  return throwError(errorMessage);
+    } else {
+      errorMessage = 'Sistemsel bir Hata'
+    }
+    return throwError(errorMessage);
   }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { AlertifyService } from '../Services/alertify.service'
 import { ProductService } from '../Services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -16,11 +17,16 @@ export class ProductComponent implements OnInit {
   title = "Ürün Listesi";
   products: Product[] = [];
 
-  constructor(private alertifyService: AlertifyService, private productSevice: ProductService) { }
+  constructor(private alertifyService: AlertifyService,
+     private productSevice: ProductService,
+     private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.productSevice.getProducts().subscribe(data =>
-      this.products = data);
+    this.activatedRoute.params.subscribe(params=>{
+      this.productSevice.getProducts(params["categoryId"]).subscribe(data =>
+        this.products = data);
+    })
+
   }
 
   addToCart(product: any) {
